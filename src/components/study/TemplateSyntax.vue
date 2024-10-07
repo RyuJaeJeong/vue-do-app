0f997d6a4c7b622cf9edeb9568c3c4d560ac2b35
financeRyu <ryoojj8998@gmail.com>
2024-10-07 22:12:29 +0900

    Template Syntax 연습

---
+++ src/components/study/TemplateSyntax.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { ref } from "vue";
+const message = ref("<h1 style='text-align: center'>Hello, world</h1>");
+const bgDanger = ref("bg-danger");
+</script>
+
+<template>
+<!--      <div v-html="message" v-bind:class="bgDanger"></div>-->
+
+      <div>
+        <p :class="(message.indexOf('<') != -1)?bgDanger:''"> {{ message.replace(/[^\w\s]/g, '') }} </p>
+      </div>
+
+</template>
+
+<style scoped>
+
+div{
+  width: 100%;
+  margin: 250px auto;
+
+}
+
+p{
+  font-size: 5rem;
+  text-align: center;
+}
+
+.bg-danger{
+  background: red !important;
+}
+
+</style>
